bf3af81eb1b344946b11589fd26da536f4ff69aa
Otryaskin Andrey <kinlydog@gmail.com>
2022-08-19 02:26:57 +1000

    Complete project

--- src/Level1.java
+++ src/Level1.java
@@ -2,7 +2,7 @@ public class Level1 {
     static String[] ma = new String[1000];
     static int i = 0;
 
-    static StringBuilder bld = new StringBuilder();
+    static StringBuilder str = new StringBuilder();
     static boolean reset = false;
 
     public static void resetHistory() {
@@ -21,14 +21,14 @@ public class Level1 {
         int n = Character.getNumericValue(command.charAt(0));
         int index = command.indexOf(" ") + 1;
 
-        String str = command.substring(index);
-        int strLen = bld.length();
+        String com = command.substring(index);
+        int strLen = str.length();
 
-        ma[i] = bld.toString();
+        ma[i] = str.toString();
 
         int strNum = 0;
         if (n == 2 || n == 3) {
-            strNum = Integer.parseInt(str);
+            strNum = Integer.parseInt(com);
         }
 
         if (reset && (n == 1 || n == 2)) {
@@ -37,41 +37,40 @@ public class Level1 {
 
         switch (n) {
             case 1:
-                bld.append(str);
                 i++;
+                str.append(com);
                 break;
 
             case 2:
-                if (strNum > strLen) {
-                    strNum = strLen;
-                }
-
-                bld.delete(strLen - strNum, strLen);
                 i++;
+                if (strNum > strLen) strNum = strLen;
+
+                str.delete(strLen - strNum, strLen);
                 break;
 
             case 3:
                 if (strNum >= strLen) return "";
 
-                return String.valueOf(bld.charAt(strNum));
+                return String.valueOf(str.charAt(strNum));
 
             case 4:
                 if (i != 0) i--;
 
-                bld.replace(0, bld.length(), ma[i]);
                 reset = true;
+
+                str.replace(0, str.length(), ma[i]);
                 break;
 
             case 5:
                 if (ma[i + 1] != null) i++;
 
-                bld.replace(0, bld.length(), ma[i]);
+                str.replace(0, str.length(), ma[i]);
                 break;
 
             default:
                 return "";
         }
 
-        return bld.toString();
+        return str.toString();
     }
 }
