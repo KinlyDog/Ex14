c0d9efa7d68843a76d1c649747bb2ee9d4a62bc8
Otryaskin Andrey <kinlydog@gmail.com>
2022-08-19 02:37:58 +1000

    Complete project

--- src/Level1.java
+++ src/Level1.java
@@ -2,7 +2,7 @@ public class Level1 {
     static String[] ma = new String[1000];
     static int i = 0;
 
-    static StringBuilder str = new StringBuilder();
+    static StringBuilder strBld = new StringBuilder();
     static boolean reset = false;
 
     public static void resetHistory() {
@@ -22,9 +22,9 @@ public class Level1 {
         int index = command.indexOf(" ") + 1;
 
         String com = command.substring(index);
-        int strLen = str.length();
+        int strLen = strBld.length();
 
-        ma[i] = str.toString();
+        ma[i] = strBld.toString();
 
         int strNum = 0;
         if (n == 2 || n == 3) {
@@ -38,39 +38,36 @@ public class Level1 {
         switch (n) {
             case 1:
                 i++;
-                str.append(com);
+                strBld.append(com);
                 break;
 
             case 2:
                 i++;
                 if (strNum > strLen) strNum = strLen;
 
-                str.delete(strLen - strNum, strLen);
+                strBld.delete(strLen - strNum, strLen);
                 break;
 
             case 3:
                 if (strNum >= strLen) return "";
 
-                return String.valueOf(str.charAt(strNum));
+                return String.valueOf(strBld.charAt(strNum));
 
             case 4:
                 if (i != 0) i--;
 
                 reset = true;
 
-                str.replace(0, str.length(), ma[i]);
+                strBld.replace(0, strBld.length(), ma[i]);
                 break;
 
             case 5:
                 if (ma[i + 1] != null) i++;
 
-                str.replace(0, str.length(), ma[i]);
+                strBld.replace(0, strBld.length(), ma[i]);
                 break;
-
-            default:
-                return "";
         }
 
-        return str.toString();
+        return strBld.toString();
     }
 }
